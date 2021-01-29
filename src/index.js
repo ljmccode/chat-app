11ae105bbe64df4676e96520627d19c1ddc112c2
Laura <lmcsay@gmail.com>
2021-01-29 17:55:55 -0600

    Refactor: Start server using http library

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
 
 const app = express()
-const port = process.env.PORT || 3000
+const server = http.createServer(app)
 
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.json())
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Now listening on port ${port}`)
-})
+}) 
