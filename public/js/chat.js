5abd2a66c4b8d2711e3b74a606b883f1016502a6
Laura <lmcsay@gmail.com>
2021-02-01 10:51:41 -0600

    Feat: Users can send out messages and location

---
+++ public/js/chat.js
@@ -0,0 +1,29 @@
+const socket = io()
+
+socket.on('welcome', (welcomeMessage) => {
+    console.log(welcomeMessage)
+})
+
+socket.on('message', (message) => {
+    console.log(`${message}`)
+})
+
+document.querySelector('#send').addEventListener('click', (e) => {
+    e.preventDefault()
+    let message = document.getElementById('message').value
+    socket.emit('sendMessage', message)
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    // if browser doesn't support geolocation
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -14,8 +14,23 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.json())
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
-    console.log('New WebSocket connection')
+io.on('connection', async (socket) => {
+    // emits event to that specific connection
+    await socket.emit('welcome', 'Welcome!')
+    // sends to everybody but this particular socket
+    socket.broadcast.emit('message', 'A new user has joined')
+    socket.on('sendMessage', (message) => {
+        // emit the event to every single connection available
+        io.emit('message', message)
+    })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.lat},${coords.long}`)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
 })
 
 server.listen(port, () => {
